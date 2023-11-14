7fce23628bb68ed6b3baa7ca564a49975b3c75be
graywes <138262543+graywes@users.noreply.github.com>
2023-11-14 09:08:57 -0800

    update 2

--- ball.gd
+++ ball.gd
@@ -6,12 +6,17 @@ const JUMP_VELOCITY = -400.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var vel = Vector2.DOWN
+var vel = Vector2(.5, .5)
 
 func _physics_process(delta):
 	velocity = vel
-	var colision = move_and_collide(velocity * delta * 70)
+	var colision = move_and_collide(velocity * delta * 130)
 	if colision:
 		print("yewa")
-		vel = velocity.bounce(colision.get_normal().rotated((colision.get_collider().get_parent().get_parent().position).angle_to(position + Vector2(0, 5)) * -2))
-		print(colision.get_collider().get_parent().get_parent())
+		if colision.get_collider().is_class("AnimatableBody2D"):
+			vel = velocity.bounce(colision.get_normal().rotated((colision.get_collider().get_parent().get_parent().position).angle_to(position + Vector2(0, 5)) * -2))
+			print(colision.get_collider().get_parent().get_parent())
+		else:
+			vel = velocity.bounce(colision.get_normal())
+			if colision.get_collider().has_method("crash"):
+				colision.get_collider().call("crash")
--- input.gd
+++ input.gd
@@ -1,5 +1,5 @@
 extends PathFollow2D
-
+var move = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,6 +10,13 @@ func _ready():
 func _process(delta):
 	if Input.is_action_pressed("left"):
 		progress_ratio -= delta * 0.8
+		move = -0.8
 	if Input.is_action_pressed("right"):
 		progress_ratio += delta * 0.8
+		move = 0.8
+	if Input.is_action_pressed("left") or Input.is_action_pressed("right"):
+		pass
+	else:
+		progress_ratio += move * delta
+		move = lerpf(move, 0, delta * 8)
 	pass
